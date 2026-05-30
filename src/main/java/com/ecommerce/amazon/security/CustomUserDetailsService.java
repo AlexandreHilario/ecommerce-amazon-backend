aa7d8de767ebb699f19450d8a80b7addf941912f
avalenca74 <adriano.00000860049@unicap.br>
2026-05-30 16:45:27 -0300

    fix(security): construcao da entidade Usuario e adaptador UserDetailsImpl

--- src/main/java/com/ecommerce/amazon/security/CustomUserDetailsService.java
+++ src/main/java/com/ecommerce/amazon/security/CustomUserDetailsService.java
@@ -1,4 +1,25 @@
 package com.ecommerce.amazon.security;
 
-public class CustomUserDetailsService {
+import com.ecommerce.amazon.repository.UsuarioRepository;
+import com.ecommerce.amazon.entity.Usuario; 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Usuario usuario = usuarioRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado com o e-mail: " + username));
+
+
+        return new UserDetailsImpl(usuario);
+    }
 }
--- src/main/java/com/ecommerce/amazon/security/JwtService.java
+++ src/main/java/com/ecommerce/amazon/security/JwtService.java
@@ -1,4 +1,50 @@
 package com.ecommerce.amazon.security;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.function.Function;
+
+@Service
 public class JwtService {
+
+    @Value("${api.security.token.secret}")
+    private String secretKey;
+
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    public String generateToken(UserDetails userDetails) {
+        return Jwts.builder()
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24)) 
+                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSignInKey() {
+        return Keys.hmacShaKeyFor(secretKey.getBytes());
+    }
 }
