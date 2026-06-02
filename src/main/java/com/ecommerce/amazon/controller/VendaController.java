68812921aa3f07e9e8d112c7354517a36f93ed26
Ana Clara Brasil <anaclaradasilvabrasil8@gmail.com>
2026-06-02 19:50:33 +0000

    v.c

--- src/main/java/com/ecommerce/amazon/controller/VendaController.java
+++ src/main/java/com/ecommerce/amazon/controller/VendaController.java
@@ -1,5 +1,40 @@
 package com.ecommerce.amazon.controller;
 
+import com.ecommerce.amazon.entity.Venda;
+import com.ecommerce.amazon.entity.enums.StatusVenda;
+import com.ecommerce.amazon.service.VendaService;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/vendas")
+@RequiredArgsConstructor
 public class VendaController {
-    
+
+    private final VendaService vendaService;
+
+    @GetMapping("/{id}")
+    public Venda buscarPorId(@PathVariable Long id) {
+
+        return vendaService.buscarPorId(id);
+    }
+
+    @GetMapping("/historico/{usuarioId}")
+    public List<Venda> buscarHistorico(
+            @PathVariable Long usuarioId) {
+
+        return vendaService.buscarHistorico(usuarioId);
+    }
+
+    @PatchMapping("/{id}/status")
+    public Venda atualizarStatus(
+            @PathVariable Long id,
+            @RequestParam StatusVenda status) {
+
+        return vendaService.atualizarStatus(id, status);
+    }
 }
