8f7d948a13db2dccb7fe05bafc9c62fe88a7dec5
Ana Clara Brasil <anaclaradasilvabrasil8@gmail.com>
2026-06-02 18:42:03 +0000

    salvando progresso atual

--- src/main/java/com/ecommerce/amazon/repository/VendaProdutoRepository.java
+++ src/main/java/com/ecommerce/amazon/repository/VendaProdutoRepository.java
@@ -2,6 +2,7 @@ package com.ecommerce.amazon.repository;
 
 import com.ecommerce.amazon.entity.VendaProduto;
 import org.springframework.data.jpa.repository.JpaRepository;
+<<<<<<< HEAD
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,3 +12,9 @@ public interface VendaProdutoRepository extends JpaRepository<VendaProduto, Long
     List<VendaProduto> findByVendaId(Long vendaId);
     List<VendaProduto> findByProdutoId(Long produtoId);
 }
+=======
+
+public interface VendaProdutoRepository
+        extends JpaRepository<VendaProduto, Long> {
+}
+>>>>>>> a60b535 (salvando progresso atual)
--- src/main/java/com/ecommerce/amazon/repository/VendaRepository.java
+++ src/main/java/com/ecommerce/amazon/repository/VendaRepository.java
@@ -1,5 +1,6 @@
 package com.ecommerce.amazon.repository;
 
+<<<<<<< HEAD
 import com.ecommerce.amazon.entity.Venda;
 import com.ecommerce.amazon.entity.enums.StatusVenda;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -12,4 +13,22 @@ public interface VendaRepository extends JpaRepository<Venda, Long> {
     List<Venda> findByUsuarioId(Long usuarioId);
     List<Venda> findByUsuarioIdAndStatus(Long usuarioId, StatusVenda status);
     List<Venda> findByStatus(StatusVenda status);
+=======
+package com.ecommerce.amazon.repository;
+
+import com.ecommerce.amazon.entity.Usuario;
+import com.ecommerce.amazon.entity.Venda;
+import com.ecommerce.amazon.entity.enums.StatusVenda;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface VendaRepository extends JpaRepository<Venda, Long> {
+
+    List<Venda> findByUsuario(Usuario usuario);
+
+    List<Venda> findByStatus(StatusVenda status);
+
+    List<Venda> findByUsuarioAndStatus(Usuario usuario, StatusVenda status);
+>>>>>>> a60b535 (salvando progresso atual)
 }
