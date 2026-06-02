0804dcd2809a0609bb4bc47751e5bdb2145e5ffa
Ana Clara Brasil <anaclaradasilvabrasil8@gmail.com>
2026-06-02 19:39:24 +0000

    Vs

--- src/main/java/com/ecommerce/amazon/repository/VendaProdutoRepository.java
+++ src/main/java/com/ecommerce/amazon/repository/VendaProdutoRepository.java
@@ -2,7 +2,6 @@ package com.ecommerce.amazon.repository;
 
 import com.ecommerce.amazon.entity.VendaProduto;
 import org.springframework.data.jpa.repository.JpaRepository;
-<<<<<<< HEAD
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,9 +11,3 @@ public interface VendaProdutoRepository extends JpaRepository<VendaProduto, Long
     List<VendaProduto> findByVendaId(Long vendaId);
     List<VendaProduto> findByProdutoId(Long produtoId);
 }
-=======
-
-public interface VendaProdutoRepository
-        extends JpaRepository<VendaProduto, Long> {
-}
->>>>>>> a60b535 (salvando progresso atual)
--- src/main/java/com/ecommerce/amazon/service/VendaService.java
+++ src/main/java/com/ecommerce/amazon/service/VendaService.java
@@ -1,11 +1,17 @@
 package com.ecommerce.amazon.service;
 
-import com.ecommerce.amazon.repository.VendaRepository;
-import com.ecommerce.amazon.repository.VendaProdutoRepository;
+import com.ecommerce.amazon.entity.Venda;
+import com.ecommerce.amazon.entity.enums.StatusVenda;
 import com.ecommerce.amazon.repository.ProdutoRepository;
 import com.ecommerce.amazon.repository.UsuarioRepository;
+import com.ecommerce.amazon.repository.VendaProdutoRepository;
+import com.ecommerce.amazon.repository.VendaRepository;
+
 import lombok.RequiredArgsConstructor;
+
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -16,4 +22,26 @@ public class VendaService {
     private final ProdutoRepository produtoRepository;
     private final UsuarioRepository usuarioRepository;
 
+    public Venda buscarPorId(Long id) {
+
+        return vendaRepository.findById(id)
+                .orElseThrow(() ->
+                        new RuntimeException("Venda não encontrada"));
+    }
+
+    public List<Venda> buscarHistorico(Long usuarioId) {
+
+        return vendaRepository.findByUsuarioId(usuarioId);
+    }
+
+    public Venda atualizarStatus(Long vendaId,
+                                 StatusVenda novoStatus) {
+
+        Venda venda = buscarPorId(vendaId);
+
+        venda.setStatus(novoStatus);
+
+        return vendaRepository.save(venda);
+    }
 }
+  
