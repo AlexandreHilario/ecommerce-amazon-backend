082b3f47e3a60caef0b2d2ac62ec14b45b9ef6bf
Ana Clara Brasil <anaclaradasilvabrasil8@gmail.com>
2026-06-02 19:26:05 +0000

    VendaS

--- src/main/java/com/ecommerce/amazon/service/VendaService.java
+++ src/main/java/com/ecommerce/amazon/service/VendaService.java
@@ -1,4 +1,19 @@
 package com.ecommerce.amazon.service;
 
+import com.ecommerce.amazon.repository.VendaRepository;
+import com.ecommerce.amazon.repository.VendaProdutoRepository;
+import com.ecommerce.amazon.repository.ProdutoRepository;
+import com.ecommerce.amazon.repository.UsuarioRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class VendaService {
+
+    private final VendaRepository vendaRepository;
+    private final VendaProdutoRepository vendaProdutoRepository;
+    private final ProdutoRepository produtoRepository;
+    private final UsuarioRepository usuarioRepository;
+
 }
