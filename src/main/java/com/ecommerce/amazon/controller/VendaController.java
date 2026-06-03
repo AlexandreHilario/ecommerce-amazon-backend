f8a3d589949dcd2993c18c099ecd790d2ec67f31
Beatriz Pagliani <beatrizsales.pcc@gmail.com>
2026-06-03 18:42:42 -0300

    fix: imports vendas

--- src/main/java/com/ecommerce/amazon/controller/VendaController.java
+++ src/main/java/com/ecommerce/amazon/controller/VendaController.java
@@ -1,3 +1,21 @@
+package com.ecommerce.amazon.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ecommerce.amazon.entity.Venda;
+import com.ecommerce.amazon.entity.enums.StatusVenda;
+import com.ecommerce.amazon.service.VendaService;
+
+import lombok.RequiredArgsConstructor;
+
 @RestController
 @RequestMapping("/vendas")
 @RequiredArgsConstructor
