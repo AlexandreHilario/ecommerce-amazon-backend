8965d161e2532a319e922f156a2543e8ddb6a134
Beatriz Pagliani <beatrizsales.pcc@gmail.com>
2026-06-05 12:39:24 -0300

    adicionar a mapper do produto

--- src/main/java/com/ecommerce/amazon/mapper/ProdutoMapper.java
+++ src/main/java/com/ecommerce/amazon/mapper/ProdutoMapper.java
@@ -1,4 +1,39 @@
 package com.ecommerce.amazon.mapper;
 
+import com.ecommerce.amazon.dto.produto.ProdutoRequestDTO;
+import com.ecommerce.amazon.dto.produto.ProdutoResponseDTO;
+import com.ecommerce.amazon.entity.Produto;
+import org.springframework.stereotype.Component;
+
+@Component
 public class ProdutoMapper {
+
+    public Produto toEntity(ProdutoRequestDTO dto) {
+        return Produto.builder()
+                .nome(dto.getNome())
+                .descricao(dto.getDescricao())
+                .preco(dto.getPreco())
+                .estoque(dto.getEstoque())
+                .ativo(dto.getAtivo())
+                .build();
+    }
+
+    public ProdutoResponseDTO toResponseDTO(Produto produto) {
+        return ProdutoResponseDTO.builder()
+                .id(produto.getId())
+                .nome(produto.getNome())
+                .descricao(produto.getDescricao())
+                .preco(produto.getPreco())
+                .estoque(produto.getEstoque())
+                .ativo(produto.getAtivo())
+                .build();
+    }
+
+    public void updateEntityFromDTO(Produto produto, ProdutoRequestDTO dto) {
+        produto.setNome(dto.getNome());
+        produto.setDescricao(dto.getDescricao());
+        produto.setPreco(dto.getPreco());
+        produto.setEstoque(dto.getEstoque());
+        produto.setAtivo(dto.getAtivo());
+    }
 }
