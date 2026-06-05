2c57f45e4694df2a5321a06de6f345e982327f29
Devalice-braz <alicebraz094@gmail.com>
2026-06-05 16:32:07 -0300

    feat: criando o controller do carrinho

--- src/main/java/com/ecommerce/amazon/dto/carrinho/AdicionarProdutoDTO.java
+++ src/main/java/com/ecommerce/amazon/dto/carrinho/AdicionarProdutoDTO.java
@@ -1,4 +1,11 @@
 package com.ecommerce.amazon.dto.carrinho;
 
+import lombok.Data;
+
+@Data
 public class AdicionarProdutoDTO {
+
+    private Long produtoId;
+    private Integer quantidade;
+
 }
--- src/main/java/com/ecommerce/amazon/dto/carrinho/AtualizarQuantidadeDTO.java
+++ src/main/java/com/ecommerce/amazon/dto/carrinho/AtualizarQuantidadeDTO.java
@@ -1,4 +1,10 @@
 package com.ecommerce.amazon.dto.carrinho;
 
+import lombok.Data;
+
+@Data
 public class AtualizarQuantidadeDTO {
+
+    private Integer quantidade;
+
 }
