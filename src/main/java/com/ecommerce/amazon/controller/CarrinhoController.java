ce87b86c431e4d7e1bc7fda012429af278dffadc
Ana Clara Brasil <anaclaradasilvabrasil8@gmail.com>
2026-06-05 23:10:17 +0000

    Minha alteração no CarrinhoController

--- src/main/java/com/ecommerce/amazon/controller/CarrinhoController.java
+++ src/main/java/com/ecommerce/amazon/controller/CarrinhoController.java
@@ -1,5 +1,9 @@
 package com.ecommerce.amazon.controller;
 
+/carrinho
 public class CarrinhoController {
-    
+    get/{idcarrinho}
+    post/additem/{idproduto}
+    post/tiraritem/{idproduto}
+    delete/{carrinhoid}
 }
