7edbf58f6985d9b433a000315815b7df54ef55a6
Ana Clara Brasil <anaclaradasilvabrasil8@gmail.com>
2026-06-06 00:37:41 +0000

    (vendas): trocando RuntimeException por BusinessException e adicionando algo em GlobalExceptionHandler

--- src/main/java/com/ecommerce/amazon/exception/GlobalExceptionHandler.java
+++ src/main/java/com/ecommerce/amazon/exception/GlobalExceptionHandler.java
@@ -1,4 +1,37 @@
 package com.ecommerce.amazon.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@RestControllerAdvice 
 public class GlobalExceptionHandler {
+
+    
+    @ExceptionHandler(BusinessException.class)
+    public ResponseEntity<Map<String, Object>> handleBusiness(BusinessException ex) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST) 
+                .body(Map.of(
+                        "erro", ex.getMessage(),
+                        "status", 400,
+                        "timestamp", LocalDateTime.now()
+                ));
+    }
+
+    // Trata qualquer outro erro inesperado
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Map<String, Object>> handleGeneric(Exception ex) {
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR) 
+                .body(Map.of(
+                        "erro", "Erro interno no servidor",
+                        "status", 500,
+                        "timestamp", LocalDateTime.now()
+                ));
+    }
 }
