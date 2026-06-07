2cbffab7a1c7f01d0aafa6f703973948604643c5
Devalice-braz <alicebraz094@gmail.com>
2026-06-07 14:10:43 -0300

    feat: adiciona validações nos DTOs do carrinho

--- src/main/java/com/ecommerce/amazon/dto/carrinho/AdicionarProdutoDTO.java
+++ src/main/java/com/ecommerce/amazon/dto/carrinho/AdicionarProdutoDTO.java
@@ -1,11 +1,16 @@
 package com.ecommerce.amazon.dto.carrinho;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class AdicionarProdutoDTO {
 
+    @NotNull(message = "O ID do produto é obrigatório")
     private Long produtoId;
-    private Integer quantidade;
 
+    @NotNull(message = "A quantidade é obrigatória")
+    @Min(value = 1, message = "A quantidade deve ser maior que zero")
+    private Integer quantidade;
 }
--- src/main/java/com/ecommerce/amazon/dto/carrinho/AtualizarQuantidadeDTO.java
+++ src/main/java/com/ecommerce/amazon/dto/carrinho/AtualizarQuantidadeDTO.java
@@ -1,10 +1,13 @@
 package com.ecommerce.amazon.dto.carrinho;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class AtualizarQuantidadeDTO {
 
+    @NotNull(message = "A quantidade é obrigatória")
+    @Min(value = 1, message = "A quantidade deve ser maior que zero")
     private Integer quantidade;
-
 }
